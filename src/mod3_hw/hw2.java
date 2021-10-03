3dc6c3ba801519d073dc47c48efb54b327fae81f
judithwilliams <judithwilliams@localhost>
2021-10-03 10:39:44 -0400

    comiint hw 3

--- src/mod3_hw/hw2.java
+++ src/mod3_hw/hw2.java
@@ -7,26 +7,97 @@ public class hw2 {
 //		1. Write Fibonnaci series between 
 //		the user entered start and end values
 		
-		int n;
+		
+		
+		int start, finish, N =10, counter = 0;
 		
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter starting Value");
-		n = scan.nextInt();
+		start= scan.nextInt();
 		
-		if(n<=1)
+		Scanner scana = new Scanner(System.in);
+		System.out.println("Enter an ending Value");
+		finish= scana.nextInt();
 		
-			System.out.println("0");
 		
+		While (counter < N); {
+			System.out.print(start + " ");
+			int fib = start + finish;
+			start =finish;
+			finish = fib;
+			counter = counter + 1;
 		}
+		
+		
+	
+	
+		
+			System.out.println("N");
+		
+		
 //
 //		2. Determine whether a user entered number 
 //		is an Armstrong number
+		
+		
+
+	
+		int num, originalNumber, remainder, result = 0;
+		
+		
+		Scanner scann = new Scanner(System.in);
+		System.out.println("Enter a number is ir an Armstrong number?");
+		num = scann.nextInt();
+		
+		originalNumber = num;
+				
+		while (originalNumber !=0)
+		{
+			remainder = originalNumber % 10;
+			result += Math.pow(remainder, 3);
+			originalNumber /= 10;
+		}
+		
+		if(result == num)
+			System.out.println(num + "is an Armstrong number");
+		else
+			System.out.println(num +"is not an Armstrong number");
+		
+	
+	
+	
+		
+		
+	
 //
 //		3. Write a program to see if a user entered 
 //		string is a palindrome or not
+		
+		
+		String str, rev ="";
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Enter a string is it a palindrome?");
+		str = sc.nextLine();
+		
+		
+		int length = str.length();
+		
+		for(int i = length - 1; i >=0; i-- )
+			rev = rev + str.charAt(i);
+		
+		
+		if(str.equals(rev))
+			System.out.println(str + "is a palindrome!!");
+		else
+			System.out.println(str + "is not a palindrome!!");
+	}
 
-
+	private static void While(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	}
 
